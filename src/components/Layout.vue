729d6c83ab9a817c533dc88513eb606abba3f0df
wanzhen <kelly811124811124@hotmail.com>
2018-06-19 00:41:44 +0800

    vue init webpack admin-order & import bootstrap & npm run build

---
+++ src/components/Layout.vue
@@ -0,0 +1,40 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Shoptime</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ORDERS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PRODUCT</a>
+      </li>
+    </ul>
+    <div style="color:#fff;">
+      ADMIN
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
